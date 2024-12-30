e379b198183179ea25047ef3be87105d596bd54b
RajChandravanshi <rajchandravanshi9695@gmail.com>
2024-12-31 00:14:31 +0530

    requirements installetion

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer"  # Removed the trailing hyphen
+AUTHOR_USER_NAME = "RajChandravanshi"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "rajchandravanshi1109@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,  # Fixed name
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small Python package for an NLP app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[],  # Add dependencies if required
+)
